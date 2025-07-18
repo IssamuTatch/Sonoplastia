1da8b90c9794d52bde70666c5ab81ed4af95a1a3
IssamuTatch <issamu.oba.matsumoto@gmail.com>
2025-07-17 23:18:54 -0300

    Change Hello audio to a Random one

--- commands/__init__.py
+++ commands/__init__.py
@@ -1,8 +1,10 @@
 from .ping import setup as setup_ping
 from .lol import setup as setup_lol
 from .echo import setup as setup_echo
+#from .lm import setup as setup_lm
 
 def setup_commands(bot):
     setup_ping(bot)
     setup_lol(bot)
     setup_echo(bot)
+    #setup_lm(bot)
--- events/on_voice_state_update.py
+++ events/on_voice_state_update.py
@@ -1,6 +1,7 @@
 import discord
 import asyncio
 import os
+import random
 
 def setup(bot):
     @bot.event
@@ -21,15 +22,19 @@ def setup(bot):
                 voice_client = await channel.connect()
                 print(f"Entering on voice channel {channel.name}...")
 
-                # Path
-                audio_path = "./sound/alo_5.mp3"
+                # Get random audio file from the sound folder
+                audio_folder_path = "./sounds/hello"
+                mp3_files = [f for f in os.listdir(audio_folder_path) if f.endswith(".mp3")]
+                print(mp3_files)
+                audio_path = audio_folder_path + "/" + random.choice(mp3_files)
+                print(audio_path)
 
                 if not os.path.exists(audio_path):
                     print("Audio file not found.")
                     await voice_client.disconnect()
                     return
 
-                # Convert to ffmpeg and play the Audio
+                # Convert the .mp3 to ffmpeg and play the Audio
                 audio_source = discord.FFmpegPCMAudio(audio_path)
                 voice_client.play(audio_source)
 
